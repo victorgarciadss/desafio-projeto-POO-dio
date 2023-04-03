a2f4b1e37441e476397cc09b40b3589f8ff5f41a
victorgarciadss <garciavictor.1308@gmail.com>
2023-04-02 22:45:26 -0300

    feat: abstracting and encapsulating classes

--- src/App.java
+++ src/App.java
@@ -1,5 +1,29 @@
+import java.time.LocalDate;
+
+import structure.projetct.domain.Course;
+import structure.projetct.domain.Mentoring;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Course course = new Course();
+        course.setTitle("Curso Java");
+        course.setDescription("Descrição do curso Java");
+        course.setWorkload(8);
+
+        Course course2 = new Course();
+        course2.setTitle("Curso JavaScript");
+        course2.setDescription("Descrição do curso JavaScript");
+        course2.setWorkload(1);
+
+        Mentoring mentoring = new Mentoring();
+        mentoring.setTitle("Mentoria de java");
+        mentoring.setDescription("descrição da mentoria de java");
+        mentoring.setDate(LocalDate.now());
+
+        System.out.println(course);
+        System.out.println(course2);
+
+        System.out.println(mentoring);
     }
 }
---
+++ src/structure/projetct/domain/Course.java
@@ -0,0 +1,45 @@
+package structure.projetct.domain;
+
+public class Course {
+    
+    private String title;
+    private String description;
+    private int workload;
+
+    public Course() {
+        
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getWorkload() {
+        return workload;
+    }
+
+    public void setWorkload(int workload) {
+        this.workload = workload;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso [título = " + title + ", descrição = " + description + ", carga horária = " + workload
+         + "]";
+    }
+
+    
+    
+}
